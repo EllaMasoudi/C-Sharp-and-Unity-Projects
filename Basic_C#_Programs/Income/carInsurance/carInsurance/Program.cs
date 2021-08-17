8a60f47c13cbc92f513ea91f25fca0d3f7020621
EllaMasoudi <86387984+EllaMasoudi@users.noreply.github.com>
2021-08-17 11:55:13 -0700

    car

---
+++ Basic_C#_Programs/Income/carInsurance/carInsurance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?");
+            bool noDUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+
+            bool isQualified = age >= 15 && noDUI=true && ticket =< 3;
+            
+            Console.WriteLine(isQualified);
+            
+            Console.ReadLine();
+        }
+    }
+}
--- Basic_C#_Programs/Income/carInsurance/carInsurance/Properties/AssemblyInfo.cs
+++ Basic_C#_Programs/Income/carInsurance/carInsurance/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("incomeComparison")]
+[assembly: AssemblyTitle("carInsurance")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("incomeComparison")]
+[assembly: AssemblyProduct("carInsurance")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8b3acabf-e932-4bd6-8636-b9e721d60bbd")]
+[assembly: Guid("4686230c-84b6-46ed-9e74-75a8a9cc7dcf")]
 
 // Version information for an assembly consists of the following four values:
 //
