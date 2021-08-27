fb149e8ba53676ffb73c70695c96af172ba5a18a
EllaMasoudi <86387984+EllaMasoudi@users.noreply.github.com>
2021-08-27 16:59:39 -0700

    Method

---
+++ Basic_C#_Programs/CallingMethod/CallingMethod/Calculate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethod
+{
+    public class Calculate
+    {
+     public int AddOne(int num)
+        {
+            return num + 1;
+
+        }
+        public int MultiplyOne(int num)
+        {
+            return num * 1;
+        }
+        public int SubtractOne(int num)
+        {
+            return num -1;
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallingMethod/CallingMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number for math operation:It will add, multiply and subtract 1 to your number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Calculate cal = new Calculate();
+            Console.WriteLine(cal.AddOne(userInput));
+            Console.WriteLine(cal.MultiplyOne(userInput));
+            Console.WriteLine(cal.SubtractOne(userInput));
+
+            Console.ReadLine();
+
+        }
+    }
+}
