5eb1f7b0eb0e441697e7a65e357976c7bb898dd4
EllaMasoudi <86387984+EllaMasoudi@users.noreply.github.com>
2021-08-27 21:56:34 -0700

    methodMAin

---
+++ Basic_C#_Programs/MainMethod/MainMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleMath sm = new SimpleMath();
+            
+            Console.WriteLine(sm.Calculate(10));
+            Console.WriteLine(sm.Calculate(11));
+            Console.WriteLine(sm.Calculate("3"));
+           
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethod/MainMethod/SimpleMath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+   public class SimpleMath
+    {
+       
+        
+   public int Calculate(int num)
+    {
+            return num + 1;
+           
+    }
+
+        public int Calculate(double num)
+        {
+            return (int)(num - 1);
+        }
+        public int Calculate(string num)
+        {
+            return Int32.Parse(num) * 2;
+        }
+
+
+
+
+    }
+}
