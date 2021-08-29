3ceb74aa19b082ddb332b6da4ad2658ec5bb5e1d
EllaMasoudi <86387984+EllaMasoudi@users.noreply.github.com>
2021-08-28 19:59:36 -0700

    optional

---
+++ Basic_C#_Programs/Method/Method/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+   public  class Math
+    {
+        public int Calculate(int num1, int num2 =10)
+        {
+            return num2 + num1;
+        }
+    }
+}
---
+++ Basic_C#_Programs/Method/Method/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Math math = new Math();
+            Console.WriteLine(math.Calculate(userInput));
+
+            Console.WriteLine("You can enter the second number for a math operation(you don't have to enter the second number ,it is optional");
+            string secondInput = Console.ReadLine();
+
+            if (secondInput!= "")
+            {
+              int  UsersecondInput = Convert.ToInt32(secondInput);
+                Console.WriteLine(math.Calculate(UsersecondInput));
+            }
+            else
+            {
+                Console.WriteLine(math.Calculate(userInput));
+            }
+      
+            Console.ReadLine();
+           
+
+            //if (2ndInput != ''){
+            //    int 2ndUserInput = Convert.ToInt32(2ndInput);
+            //}
+            //Console.WriteLine(userInput);
+            //Math math = new Math();
+            //Console.WriteLine(math.Calculate(userInput));
+        }
+    }
+}
