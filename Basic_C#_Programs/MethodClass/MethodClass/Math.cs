4222a041924fd046e682af8298171472a0bcdaec
EllaMasoudi <86387984+EllaMasoudi@users.noreply.github.com>
2021-08-28 20:31:13 -0700

    void

---
+++ Basic_C#_Programs/MethodClass/MethodClass/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+   public class Math
+    {
+        public void Calculate(int num1, int num2)
+        {
+            num1 = num1 + 1;
+            Console.WriteLine(num2);
+
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/MethodClass/MethodClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            math.Calculate(3,1);
+            int a = 12;
+            int b = 6;
+            math.Calculate(a, b);
+        }
+        
+    }
+}
