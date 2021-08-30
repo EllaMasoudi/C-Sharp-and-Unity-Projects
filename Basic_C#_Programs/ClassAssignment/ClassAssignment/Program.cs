59226b75b64e379e38d497826f1bc004c25cbd43
EllaMasoudi <86387984+EllaMasoudi@users.noreply.github.com>
2021-08-29 23:34:10 -0700

    assignment

---
+++ Basic_C#_Programs/ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please enter a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int r;
+            int j = 5;
+            math.Calculate(userInput, out j);
+            Console.WriteLine(j);
+            Console.ReadLine();
+
+        }
+    }
+}
