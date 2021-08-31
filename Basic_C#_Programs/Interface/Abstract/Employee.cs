ffd796f929b4db66ac59480b3c2cd260747a1cd0
EllaMasoudi <86387984+EllaMasoudi@users.noreply.github.com>
2021-08-31 00:21:35 -0700

    interface

---
+++ Basic_C#_Programs/Interface/Abstract/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+  public  class Employee: Person, IQuittable
+    {
+        public override void sayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+            
+        }
+        public void Quit()
+        {
+            Console.WriteLine("quit is called");
+        }
+    }
+    
+}
