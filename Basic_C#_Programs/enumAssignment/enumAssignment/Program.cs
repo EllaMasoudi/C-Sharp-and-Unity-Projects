8a4d809ce2d2ccb17cdb2551f1ec5852c59c3e75
EllaMasoudi <86387984+EllaMasoudi@users.noreply.github.com>
2021-09-01 00:21:44 -0700

    enum

---
+++ Basic_C#_Programs/enumAssignment/enumAssignment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumAssignment
+{
+    public enum Weekdays
+    {
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednsday,
+        Thursday,
+        Friday
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week:");
+            String UserInput = Console.ReadLine();
+            bool Done = false;
+            while (!Done)
+            {
+
+
+                try
+                {
+
+                    Weekdays today = (Weekdays)Enum.Parse(typeof(Weekdays), UserInput);
+                    Console.WriteLine(today);
+                    Done = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please enter an actual day of the week.");
+                }
+
+                //foreach (string dayName in Enum.GetNames(typeof(Weekdays)))
+                //{
+                //    Console.WriteLine("{0} = {1:D}", dayName,
+                //                                 Enum.Parse(typeof(Weekdays), dayName));
+                //}
+                UserInput = Console.ReadLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+
+
+}
